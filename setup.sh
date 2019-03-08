8a992b1bd777d419ce13c61907bab9909d87b2f0
Kevin Reilly <kevinreilly@pop-os.localdomain>
2019-03-07 18:57:17 -0700

    Added setup unzip, analysis

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 mkdir data
-wget https://resources.lendingclub.com/LoanStats_2018Q4.csv.zip -o data/LoanStats_2018Q4.csv.zip &
+wget https://resources.lendingclub.com/LoanStats_2018Q4.csv.zip -O data/LoanStats_2018Q4.csv.zip &
+unzip data/LoanStats_2018Q4.csv.zip -d data
