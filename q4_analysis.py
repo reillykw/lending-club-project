5413ea0f601fb6872ceeb4486147d27bf04a32c5
Kevin Reilly <kevinreilly@pop-os.localdomain>
2019-03-07 19:27:15 -0700

    Initial entropy analysis

--- q4_analysis.py
+++ q4_analysis.py
@@ -1,4 +1,20 @@
 import pandas as pd
-from sklearn.stats import entropy
+from scipy.stats import entropy
+from collections import Counter
+from pprint import PrettyPrinter
 
-DATAFILE = 'data/'
+pp = PrettyPrinter()
+
+DATAFILE = 'data/LoanStats_2018Q4.csv'
+
+loan_data = pd.read_csv(DATAFILE, sep=",", skiprows=1, nrows=10000)
+
+entropies = dict()
+
+for column in loan_data.columns:
+    print(f'{column} ', end="")
+    counter = Counter(loan_data[column])
+    entropies[column] = entropy(list(counter.values()))
+
+print()
+print(pp.pprint(entropies))
