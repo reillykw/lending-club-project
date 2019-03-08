ab92928d9ba51faf4d0132f6535449c02e637141
Kevin Reilly <kevinreilly@pop-os.localdomain>
2019-03-07 19:47:48 -0700

    Added entropies

--- q4_analysis.py
+++ q4_analysis.py
@@ -1,20 +1,32 @@
 import pandas as pd
+from pandas import Series
 from scipy.stats import entropy
 from collections import Counter
 from pprint import PrettyPrinter
+from multiprocessing import Pool
+
+MAX_PROCESSORS = 10
 
 pp = PrettyPrinter()
 
 DATAFILE = 'data/LoanStats_2018Q4.csv'
-
 loan_data = pd.read_csv(DATAFILE, sep=",", skiprows=1, nrows=10000)
-
 entropies = dict()
+results = dict()
+pool = Pool(MAX_PROCESSORS)
 
-for column in loan_data.columns:
-    print(f'{column} ', end="")
-    counter = Counter(loan_data[column])
-    entropies[column] = entropy(list(counter.values()))
 
-print()
-print(pp.pprint(entropies))
+def calc_entropy(column):
+    counter = Counter(column)
+    return column, entropy(list(counter.values()))
+
+
+if __name__ == "__main__":
+    for column in loan_data.columns:
+        result = pool.apply_async(Counter, [loan_data[column]])
+        results[column] = result
+
+    for column, result in results.items():
+        entr = result.get()
+        entropies[column] = entropy(list(entr.values()))
+    print(pp.pprint(entropies))
