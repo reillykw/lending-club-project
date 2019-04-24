f10b8dc0982863afa0c49cfe617e3357a7ba7c69
Kevin Reilly <reilly.w.kevin@gmail.com>
2019-04-23 19:01:42 -0600

    Download bash script

--- setup.sh
+++ setup.sh
@@ -4,5 +4,18 @@ source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 mkdir data
-wget https://resources.lendingclub.com/LoanStats_2018Q4.csv.zip -O data/LoanStats_2018Q4.csv.zip &
-unzip data/LoanStats_2018Q4.csv.zip -d data
+years=(16 17 18)
+#for y in ${years[*]};
+#do
+#    for q in {1..4}
+#    do
+#        wget https://resources.lendingclub.com/LoanStats_20${y}Q${q}.csv.zip -O data/LoanStats_20${y}Q${q}.csv.zip
+#        unzip data/LoanStats_20${y}Q${q}.csv.zip -d data
+#    done
+#done
+for l in {a..d}
+do
+    wget https://resources.lendingclub.com/LoanStats3${l}.csv.zip -O data/LoanStats3${l}.csv.zip
+    unzip data/LoanStats3${l}.csv.zip -d data
+done
+rm data/*.zip
