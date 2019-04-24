5faa641f047fd0e39c39b294758acb2b2b6cb66b
Kevin Reilly <kevinreilly@pop-os.localdomain>
2019-04-23 18:18:14 -0700

    Added reqs

--- setup.sh
+++ setup.sh
@@ -5,14 +5,14 @@ pip install --upgrade pip
 pip install -r requirements.txt
 mkdir data
 years=(16 17 18)
-#for y in ${years[*]};
-#do
-#    for q in {1..4}
-#    do
-#        wget https://resources.lendingclub.com/LoanStats_20${y}Q${q}.csv.zip -O data/LoanStats_20${y}Q${q}.csv.zip
-#        unzip data/LoanStats_20${y}Q${q}.csv.zip -d data
-#    done
-#done
+for y in ${years[*]};
+do
+    for q in {1..4}
+    do
+        wget https://resources.lendingclub.com/LoanStats_20${y}Q${q}.csv.zip -O data/LoanStats_20${y}Q${q}.csv.zip
+        unzip data/LoanStats_20${y}Q${q}.csv.zip -d data
+    done
+done
 for l in {a..d}
 do
     wget https://resources.lendingclub.com/LoanStats3${l}.csv.zip -O data/LoanStats3${l}.csv.zip
